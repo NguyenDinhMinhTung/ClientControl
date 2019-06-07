5f377e1ed2dd17ead50cc708fb728f0c61be6017
NguyenDinhMinhTung <megasharesdotcom@gmail.com>
2019-06-07 12:28:24 +0900

    create

---
+++ ClientControl/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientControl
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const String serverIP = "14.9.118.64";
+        private const int serverPort = 8530;
+
+        private const int localPort = 7574;
+
+        UDPProtocol udpProtocol;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            udpProtocol = new UDPProtocol(localPort);
+            udpProtocol.UdpSocketReceiveStart(RunCommand);
+            SendIPPort();
+        }
+
+        public void RunCommand(IPEndPoint ipEndPoint, byte[] data)
+        {
+
+        }
+
+        public void SendIPPort()
+        {
+            udpProtocol.UdpSocketSend(serverIP, serverPort, new byte[] { 3, 1 });
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (udpProtocol != null)
+            {
+                udpProtocol.Close();
+            }
+        }
+    }
+}
---
+++ ClientControl/UDPProtocol.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientControl
+{
+    class UDPProtocol
+    {
+        private System.Net.Sockets.UdpClient udpClient;
+
+        private int localPort;
+        public UDPProtocol(int localPort)
+        {
+            this.localPort = localPort;
+
+            System.Net.IPAddress localIpAddress = System.Net.IPAddress.Parse(GetLocalIPAddress());
+
+            System.Net.IPEndPoint localEP = new System.Net.IPEndPoint(localIpAddress, localPort);
+
+            udpClient = new System.Net.Sockets.UdpClient(localEP);
+        }
+
+        public void UdpSocketSend(IPEndPoint iPEndPoint, byte[] data)
+        {
+            udpClient.Send(data, data.Length, iPEndPoint);
+        }
+
+        public void UdpSocketSend(String host, int port, byte[] data)
+        {
+            udpClient.Send(data, data.Length, host, port);
+        }
+
+        public void UdpSocketReceiveStart(Action<IPEndPoint, byte[]> action)
+        {
+            System.Net.IPEndPoint remoteEP = null;
+
+            Thread socketReceiveThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    byte[] receiveBytes = udpClient.Receive(ref remoteEP);
+                    Console.WriteLine("{0} {1}", remoteEP.Address, remoteEP.Port);
+                    action(remoteEP, receiveBytes);
+                    //Environment.Exit(0);
+                }
+            });
+
+            socketReceiveThread.IsBackground = true;
+            socketReceiveThread.Start();
+
+
+            //udpClient.Close();
+        }
+
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public void Close()
+        {
+            udpClient.Close();
+            udpClient.Dispose();
+        }
+    }
+}
