cf5bf268bdca360d25a8693f1a499a2c1c9aa4fa
NguyenDinhMinhTung <megasharesdotcom@gmail.com>
2019-06-13 17:33:44 +0900

    xx

---
+++ ClientControl/manager/FileExplorerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientControl.manager
+{
+    class FileExplorerManager
+    {
+        private UDPProtocol udpProtocol;
+
+        private List<FileExplorerWindow> listFileExplorerWindow;
+
+        public FileExplorerManager(UDPProtocol udpProtocol)
+        {
+            this.udpProtocol = udpProtocol;
+            listFileExplorerWindow = new List<FileExplorerWindow>();
+        }
+    }
+}
